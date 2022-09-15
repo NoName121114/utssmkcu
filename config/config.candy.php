c1a2e7648705e2cea3cd898fc83c15a7876e47f2
NoName121114 <92351544+NoName121114@users.noreply.github.com>
2022-09-15 21:46:00 +0700

    first

---
+++ config/config.candy.php
@@ -0,0 +1,9 @@
+<?php
+$copyright = 'CBT SMK CENDIKIA UTAMA | Powered by TKJ';
+
+define("VERSI", "2.9.2");
+define("REVISI", " 3");
+define("APLIKASI", "XCBT | SMKCU");
+define("NAMA_DATABASE", "cbtcandy28");
+define("VERSI_DB", "2.9.2");
+define("BASEPATH", "cbtcandy");
---
+++ panel/pages/word_import/import/application/config/sq_config.php
@@ -0,0 +1,7 @@
+<?php
+//include "../../../../config/config.database.php";
+$sq_base_url = '';
+$sq_hostname = 'localhost';
+$sq_dbname = 'smkcukng_uts';
+$sq_dbusername = 'smkcukng_NoName';
+$sq_dbpassword = 'NoName1211';
